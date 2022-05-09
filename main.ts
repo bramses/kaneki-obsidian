f741d62bef8b7f102515c8454f9839b2b891e59a
Bram Adams <3282661+bramses@users.noreply.github.com>
2022-05-08 19:07:13 -0500

    main manifest package-lock package styles

--- main.ts
+++ main.ts
@@ -1,81 +1,126 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, request } from 'obsidian';
 
 interface MyPluginSettings {
 	mySetting: string;
+	rootPath: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	mySetting: 'default',
+	rootPath: ''
 }
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
+	getFrontmatter () {
+		const file = this.app.workspace.getActiveFile();
+		let fm = this.app.metadataCache.getFileCache(file)?.frontmatter;
+		
+		if (fm === undefined) {
+			new Notice('No frontmatter found.');
+			return;
+		}
+
+		if (fm.slug === undefined) {
+			new Notice('No slug found.');
+			return;
+		}
+
+		return fm;
+	}
+
+
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
+		
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+			id: 'update-kaneki-file',
+			name: 'Send to Kaneki',
+			editorCallback: async (editor: Editor) => {
+				
+				try {
+					if (this.settings.rootPath === '') {
+						new Notice('Please set the root path in the settings.');
+						return;
+					}
+					
+					
+					const filePath = this.app.workspace.getActiveFile().path;
+
+					const fm = this.getFrontmatter();
+
+					if (fm.title === undefined) {
+						new Notice('No title found.');
+						return;
 					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+					if (fm.excerpt === undefined) {
+						new Notice('No excerpt found.');
+						return;
+					}
+
+					const statusBarItemEl = this.addStatusBarItem();
+					statusBarItemEl.setText('Calling Kaneki...');
+					await request({
+						url: 'http://localhost:3595/update',
+						method: 'POST',
+						contentType: 'application/json',
+						body: JSON.stringify({
+							filePath: this.settings.rootPath + filePath,
+						})
+					})
+					statusBarItemEl.setText('');
+					new Notice(`${filePath} successfully sent to Kaneki.`);
+				} catch (err) {
+					new Notice(err);
+					return;
+				}
+			}
+		});
+
+		this.addCommand({
+			id: 'open-kaneki-file',
+			name: 'Open in Chrome',
+			editorCallback: async (editor: Editor) => {
+				
+				try {
+					if (this.settings.rootPath === '') {
+						new Notice('Please set the root path in the settings.');
+						return;
+					}
+
+					const fm = this.getFrontmatter();
+
+					if (fm.slug === undefined) {
+						new Notice('No slug found.');
+						return;
+					}
+					
+					const filePath = this.app.workspace.getActiveFile().path;	
+	
+					const statusBarItemEl = this.addStatusBarItem();
+					statusBarItemEl.setText('Calling Kaneki...');
+					await request({
+						url: 'http://localhost:3595/open',
+						method: 'POST',
+						contentType: 'application/json',
+						body: JSON.stringify({
+							slug: fm.slug,
+						})
+					})
+					statusBarItemEl.setText('');
+					new Notice(`${filePath} successfully opened with Kaneki.`);
+				} catch (err) {
+					new Notice(err);
+					return;
 				}
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -88,22 +133,6 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
 	}
 }
 
@@ -123,15 +152,15 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		.setName('Obsidian Absolute Root Path')
+		.setDesc('The absolute path to the Obsidian folder.')
+		.addText(text => text
+			.setPlaceholder('/some/path/to/obsidian')
+			.setValue(this.plugin.settings.rootPath)
+			.onChange(async (value) => {
+				console.log('Root Path: ' + value);
+				this.plugin.settings.rootPath = value;
+				await this.plugin.saveSettings();
+			}));
 	}
 }
